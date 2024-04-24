8ef27af44a11b47110d911e403b09b6a5f28fa7d
engchina <engchina>
2024-04-24 21:53:35 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ginza-api
 GiNZA API
+
+## Prepare
+
+```
+conda create -n ginza-api python=3.11 -y
+conda activate ginza-api
+```
+
+```
+pip install -r requirements.txt
+```
+
+## Run
+
+```
+uvicorn main:app --reload --host 0.0.0.0 --port 7932
+```
+
---
+++ main.py
@@ -0,0 +1,31 @@
+import re
+
+import spacy
+from fastapi import FastAPI
+
+app = FastAPI()
+
+nlp_ja_model = spacy.load('ja_ginza_electra')
+nlp_en_model = spacy.load('en_core_web_lg')
+
+
+@app.post("/split-query/")
+async def split_query(query_text: str):
+    """
+    Extracts relevant search texts from the input query text based on specific token tags.
+    No '名詞-数詞'.
+
+    Args:
+    - query_text (str): The input query text to extract search texts from.
+
+    Returns:
+    - list: A list of search texts extracted from the input query text.
+    """
+    ja_split_queries = list(set([token.text for token in nlp_ja_model(query_text) if
+                                 any(token.tag_.startswith(tag) for tag in
+                                     ['名詞-普通名詞', '名詞-固有名詞', '動詞-一般'])]))
+    english_words = [text for text in ja_split_queries if re.match(r'^[a-zA-Z]', text)]
+    en_split_queries = list(
+        set([token.text for token in nlp_en_model(' '.join(english_words)) if token.pos_ in ['PROPN', 'NOUN', 'VERB']]))
+    final_split_queries = list(set(ja_split_queries).difference(english_words).union(en_split_queries))
+    return final_split_queries
